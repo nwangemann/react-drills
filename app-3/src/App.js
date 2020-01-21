eb9d48113c2e6b9841b3b7a4db7ecb347d12095b
Nate <nathan.wangemann@aggiemail.usu.edu>
2020-01-21 16:51:16 -0700

    completes number 3

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import { unstable_batchedUpdates } from 'react-dom';
 
-function App() {
-  return (
+class App extends Component {
+  constructor(){
+    super()
+
+    this.state = {
+      groceries: ['Bananas', 'Apples', 'Grapes', 'Oranges', 'Lemons', 'Limes', 'Celery', 'Bread', 'Bagels', 'Raisins'],
+      filterTerm: '',
+      filterArray: ['Bananas', 'Apples', 'Grapes', 'Oranges', 'Lemons', 'Limes']
+    } 
+  }
+
+
+  update = (val) => {
+    let filterArray = []
+    for(let i = 0; i < this.state.groceries.length; i++){
+      if ((this.state.groceries[i].toLowerCase()).includes(val.toLowerCase()) === true){
+        filterArray.push(<li>{this.state.groceries[i]}</li>)
+      }
+    }
+    this.setState({
+      filterArray: filterArray
+    })
+  }
+
+
+  render(){
+      return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input onChange={(e) => this.update(e.target.value)}></input>
+      <span>{this.state.filterArray}</span>
     </div>
-  );
+     );
+  }
 }
 
 export default App;
