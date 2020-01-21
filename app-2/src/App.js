e551f80b62fb7375ba321df9ce3f389981f6eab1
Nate <nathan.wangemann@aggiemail.usu.edu>
2020-01-21 16:25:26 -0700

    completes app-2

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      menu: ["Sushi", "Burritos", "Pizza", "Escargo", "Cannoli"]
+    };
+  }
+
+  listMaker = () => {
+
+  }
+
+  render() {
+    let menuMap = this.state.menu.map((elem) => {
+      return <li>{elem}</li>;
+    })
+    return (
+     <div className="App">
+       <ol>{menuMap}</ol>
+     </div>
+    )
+  }
 }
 
 export default App;
