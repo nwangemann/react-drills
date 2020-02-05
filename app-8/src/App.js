56f8c9465bbedacfec3a4e5308a7b46d820fb18f
Nate <nathan.wangemann@aggiemail.usu.edu>
2020-02-05 16:39:29 -0700

    finishes 8

--- app-8/src/App.js
+++ app-8/src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
+class App extends Component {
+  constructor(){
+    super()
+
+    this.state = {
+      name: '',
+      height: '',
+      mass: '',
+      hair_color: '',
+      skin_color: '',
+      eye_color: ''
+    }
+  }
+
+  componentDidMount(){
+    axios.get("https://swapi.co/api/people/11").then(res => {
+      this.setState({
+        name: res.data.name,
+        height: res.data.height,
+        mass: res.data.mass,
+        hair_color: res.data.hair_color,
+        skin_color: res.data.skin_color,
+        eye_color: res.data.eye_color
+      })
+    })
+  }
+
+
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{this.state.name}</p>
+      <p>{this.state.height}</p>
+      <p>{this.state.mass}</p>
+      <p>{this.state.hair_color}</p>
+      <p>{this.state.skin_color}</p>
+      <p>{this.state.eye_color}</p>
+
     </div>
   );
+  }
 }
 
 export default App;
